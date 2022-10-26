1782e513da0656a3a542fe83f06d09defa752e73
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2022-10-26 12:25:21 +0300

    Add files via upload

--- Accounts.java
+++ Accounts.java
@@ -1,5 +1,5 @@
 abstract class Accounts {
-    public abstract double convert();
+    public abstract double convert(String option);
     public abstract void transaction ();
     public abstract String toString();
     public abstract void total();
