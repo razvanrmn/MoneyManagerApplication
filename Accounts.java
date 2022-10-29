37291dd3c7e24ce12361583cfc421d6b0caad3c5
Boglarka Molnar <114649140+boglarkaa@users.noreply.github.com>
2022-10-29 21:26:04 +0300

    Update Accounts.java

--- Accounts.java
+++ Accounts.java
@@ -1,5 +1,4 @@
 abstract class Accounts {
-    public abstract double convert(String option);
     public abstract void transaction ();
     public abstract String toString();
     public abstract void total();
