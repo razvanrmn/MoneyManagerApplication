af82a817240e0d99699bfbbe0fd018c242282d56
Boglarka Molnar <114649140+boglarkaa@users.noreply.github.com>
2022-10-31 13:19:35 +0200

    Update Accounts.java

--- Accounts.java
+++ Accounts.java
@@ -1,5 +1,15 @@
-abstract class Accounts {
+abstract class Accounts implements Cont{
+    private double amount;
+    private String category;
+    private String account;
+
+    public Accounts(){
+        amount=-1;
+        category="Null";
+        account="Null";
+    }
     public abstract void transaction ();
     public abstract String toString();
     public abstract void total();
+
 }
